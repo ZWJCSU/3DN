43d5870bbb8e20adcde7890895bec3451c7b4e7b
wenjie zhu <43605711+ZWJCSU@users.noreply.github.com>
2020-12-30 23:35:33 +0800

    Update tf_nndistance_compile.sh

--- models/tf_ops/nn_distance/tf_nndistance_compile.sh
+++ models/tf_ops/nn_distance/tf_nndistance_compile.sh
@@ -1,3 +1,3 @@
 /usr/local/cuda/bin/nvcc tf_nndistance_g.cu -o tf_nndistance_g.cu.o -c -O2 -DGOOGLE_CUDA=1 -x cu -Xcompiler -fPIC
-g++ -std=c++11 tf_nndistance.cpp tf_nndistance_g.cu.o -o tf_nndistance_so.so -shared -fPIC -I /home/amax/.local/lib/python2.7/site-packages/tensorflow/include -I /usr/local/cuda/include -I /home/amax/.local/lib/python2.7/site-packages/tensorflow/include/external/nsync/public -lcudart -L /usr/local/cuda/lib64/ -L/home/amax/.local/lib/python2.7/site-packages/tensorflow -ltensorflow_framework -O2 -D_GLIBCXX_USE_CXX11_ABI=0
+g++ -std=c++11 tf_nndistance.cpp tf_nndistance_g.cu.o -o tf_nndistance_so.so -shared -fPIC -I /home/amax/.local/lib/python2.7/site-packages/tensorflow/include -I /usr/local/cuda-9.2/include -I /home/amax/.local/lib/python2.7/site-packages/tensorflow/include/external/nsync/public -lcudart -L /usr/local/cuda-9.2/lib64/ -L/home/amax/.local/lib/python2.7/site-packages/tensorflow -ltensorflow_framework -O2 -D_GLIBCXX_USE_CXX11_ABI=0
 
