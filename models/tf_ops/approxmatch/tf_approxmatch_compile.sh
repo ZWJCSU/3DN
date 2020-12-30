047ad92d6e780502f97f6c8066051828d668a26a
wenjie zhu <43605711+ZWJCSU@users.noreply.github.com>
2020-12-30 22:33:44 +0800

    Update tf_approxmatch_compile.sh

--- models/tf_ops/approxmatch/tf_approxmatch_compile.sh
+++ models/tf_ops/approxmatch/tf_approxmatch_compile.sh
@@ -6,4 +6,4 @@
 
 # TF1.10
 /usr/local/cuda/bin/nvcc tf_approxmatch_g.cu -o tf_approxmatch_g.cu.o -c -O2 -DGOOGLE_CUDA=1 -x cu -Xcompiler -fPIC
-g++ -std=c++11 tf_approxmatch.cpp tf_approxmatch_g.cu.o -o tf_approxmatch_so.so -shared -fPIC -I /usr/local/lib/python2.7/dist-packages/tensorflow/include -I /usr/local/cuda/include -I /usr/local/lib/python2.7/dist-packages/tensorflow/include/external/nsync/public -lcudart -L /usr/local/cuda/lib64/ -L/usr/local/lib/python2.7/dist-packages/tensorflow -ltensorflow_framework -O2 -D_GLIBCXX_USE_CXX11_ABI=0
+g++ -std=c++11 tf_approxmatch.cpp tf_approxmatch_g.cu.o -o tf_approxmatch_so.so -shared -fPIC -I /home/amax/.local/lib/python2.7/site-packages/tensorflow/include -I /usr/local/cuda/include -I /home/amax/.local/lib/python2.7/site-packages/tensorflow/include/external/nsync/public -lcudart -L /usr/local/cuda/lib64/ -L/home/amax/.local/lib/python2.7/site-packages/tensorflow -ltensorflow_framework -O2 -D_GLIBCXX_USE_CXX11_ABI=0
